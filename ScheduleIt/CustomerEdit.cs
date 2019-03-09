ea446e20a77206d7c439535fe80dc52c127d4f8c
Clint Broadhead <clint@broadhead.co>
2019-03-08 23:03:11 -0700

    Add and Delete customers working, modifying existing customer still not working.

--- ScheduleIt/CustomerEdit.cs
+++ ScheduleIt/CustomerEdit.cs
@@ -13,14 +13,14 @@ namespace ScheduleIt
 {
     public partial class CustomerEdit : Form
     {
-        private customer custToSave { get; set; }
+        private customer Customer { get; set; }
         private bool newCust = false;
 
         public CustomerEdit(customer cust)
         {
             InitializeComponent();
-            this.custToSave = cust;
-            if ( this.custToSave.customerId == 0 )
+            this.Customer = cust;
+            if ( this.Customer.customerId == 0 )
             {
                 custAddLabel.Visible = true;
                 custModifyLabel.Visible = false;
@@ -31,41 +31,40 @@ namespace ScheduleIt
                 custAddLabel.Visible = false;
                 custModifyLabel.Visible = true;
 
-                int custIdToEdit = int.Parse(cust.customerId.ToString());
-                customer customerToEdit = DataAccess.GetCustomer(custIdToEdit);
-                custIdBox.Text = customerToEdit.customerId.ToString();
-                //addressIdBox.Text = custInfo.Select(c => c.AddressId).ToString();
-                //cityIdBox.Text = custInfo.Select(c => c.CityId).ToString();
-                //countryIdBox.Text = custInfo.Select(c => c.CountryId).ToString();
-
-                nameBox.Text = customerToEdit.customerName.ToString();
-                //phoneBox.Text = custInfo.Select(c => c.Phone).ToString();
-                //addressBox.Text = custInfo.Select(c => c.Address1).ToString();
-                //address2Box.Text = custInfo.Select(c => c.Address2).ToString();
-                //cityBox.Text = custInfo.Select(c => c.City).ToString();
-                //postalCodeBox.Text = custInfo.Select(c => c.PostalCode).ToString();
-                //countryBox.Text = custInfo.Select(c => c.Country).ToString();
+                customerActive.Checked = this.Customer.active;
+                nameBox.Text = this.Customer.customerName;
+                phoneBox.Text = this.Customer.address.phone;
+                addressBox.Text = this.Customer.address.address1;
+                address2Box.Text = this.Customer.address.address2;
+                cityBox.Text = this.Customer.address.city.city1;
+                postalCodeBox.Text = this.Customer.address.postalCode;
+                countryBox.Text = this.Customer.address.city.country.country1;
             }
         }
 
         private void customerSave_Click(object sender, EventArgs e)
         {
+            this.Customer.customerName = nameBox.Text;
+            this.Customer.active = customerActive.Checked;
+            this.Customer.address = GetAddress();
+            this.Customer.address.city = GetCity();
+            this.Customer.address.city.country = GetCountry();
+            this.Customer.lastUpdateBy = "test";
+
             if( this.newCust )
             {
-                this.custToSave.customerName = nameBox.Text;
-                this.custToSave.active = customerActive.Checked;
-                this.custToSave.createdBy = "test";
-                this.custToSave.createDate = DateTime.UtcNow;
-                this.custToSave.lastUpdate = DateTime.UtcNow;
-                this.custToSave.lastUpdateBy = "test";
-                this.custToSave.address = GetAddress();
-                this.custToSave.address.city = GetCity();
-                this.custToSave.address.city.country = GetCountry();
+                this.Customer.createdBy = "test";
+                this.Customer.createDate = DateTime.UtcNow;
+                this.Customer.lastUpdate = DateTime.UtcNow;
+            }
+            else
+            {
+                this.Customer.lastUpdate = DateTime.UtcNow;
             }
             
             try
             {
-                DataAccess.SaveCustomer(this.custToSave);
+                DataAccess.SaveCustomer(this.Customer);
                 this.Close();
             }
             catch (Exception ex)
@@ -78,44 +77,60 @@ namespace ScheduleIt
         {
             address addr = new address
             {
+                addressId = this.Customer.addressId,
                 address1 = addressBox.Text,
                 address2 = address2Box.Text,
                 postalCode = postalCodeBox.Text,
                 phone = phoneBox.Text,
-                createdBy = "test",
-                createDate = DateTime.UtcNow,
                 lastUpdate = DateTime.UtcNow,
-                lastUpdateBy = "test"
+                lastUpdateBy = "test",
+                cityId = this.Customer.address.cityId
             };
+
+            if ( this.newCust)
+            {
+                addr.createdBy = "test";
+                addr.createDate = DateTime.UtcNow;
+            }
 
             return addr;
         }
 
         private city GetCity()
         {
-            city city = new city
+            city City = new city
             {
+                cityId = this.Customer.address.cityId,
                 city1 = cityBox.Text,
-                createdBy = "test",
-                createDate = DateTime.UtcNow,
                 lastUpdate = DateTime.UtcNow,
-                lastUpdateBy = "test"
+                lastUpdateBy = "test",
+                countryId = this.Customer.address.city.countryId
             };
 
+            if( this.newCust)
+            {
+                City.createdBy = "test";
+                City.createDate = DateTime.UtcNow;
+            }
 
-            return city;
+            return City;
         }
 
         private country GetCountry()
         {
             country country = new country
             {
+                countryId = this.Customer.address.city.countryId,
                 country1 = countryBox.Text,
-                createdBy = "test",
-                createDate = DateTime.UtcNow,
                 lastUpdate = DateTime.UtcNow,
                 lastUpdateBy = "test"
             };
+
+            if( this.newCust)
+            {
+                country.createdBy = "test";
+                country.createDate = DateTime.UtcNow;
+            }
 
             return country;
         }
