891b79c2ec4552ed900c3fda9eeb8770a5bbe454
Clint Broadhead <clint@broadhead.co>
2019-03-17 19:20:52 -0600

    created a globalvar class to store variables used throughout the program

---
+++ ScheduleIt/GlobalVar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleIt
+{
+    public static class GlobalVar
+    {
+        public static class LoggedInUser
+        {
+            private static string _Username { get; set; }
+            private static int _UserId { get; set; }
+
+            public static string Username
+            {
+                get { return _Username; }
+                set { _Username = value; }
+            }
+
+            public static int UserId
+            {
+                get { return _UserId; }
+                set { _UserId = value; }
+            }
+        }
+        
+    }
+}
